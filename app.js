94eaaeaa7bf2b40d48cb789b68a6386c8f49d72c
KaseKreative <KaseKreative@fullsail.edu>
2015-10-02 19:41:55 -0400

    main and home page

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express 	= require('express'),
-	app 		= express();
-var	port        = process.env.PORT || 3000;
+	app 		= express(),
+	exphbs      = require('express-handlebars'),
+	port        = process.env.PORT || 3000;
 	// Environmental variable
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
  
 app.get('/derp', function (req, res) {
   res.send('Hello World');
