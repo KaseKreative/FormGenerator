b97ea16847502269b677455118e652a4ec9c8ef5
KaseKreative <KaseKreative@fullsail.edu>
2015-10-03 14:55:32 -0400

    last updates

--- Public/angular.js
+++ Public/angular.js
@@ -1,6 +1,23 @@
-var app = angular.module('formGen', []);//Something here
+var app = angular.module('formGen', ["ui.router"])
+		   
+		.config(['',function() {
+			$stateProvider.state('',{
+				url:"",
+				controller: 'mainController as main',
+				templateUrl: 'templates/template.html'
+					
+				}])
+			})	
+		}]) 
+
+	 	.controller('mainController', function(){
+
+ 	})
 
 app.config(['$interpolateProvider',function ($interpolateProvider){
 	$interpolateProvider.startSymbol('{[{');
 	$interpolateProvider.endSymbol('}]}');
 }])
+
+
+
