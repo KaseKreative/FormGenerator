cf5ae2b6890155d93eacf72b73ef3d3ae803f624
KaseKreative <KaseKreative@fullsail.edu>
2015-10-03 15:34:32 -0400

    transferring data between controllers

--- Public/angular.js
+++ Public/angular.js
@@ -1,18 +1,32 @@
 var app = angular.module('formGen', ["ui.router"])
 		   
-		.config(['',function() {
-			$stateProvider.state('',{
+		.config(function config($stateProvider) {
+			$stateProvider.state('index',{
 				url:"",
 				controller: 'mainController as main',
 				templateUrl: 'templates/template.html'
-					
-				}])
+				})
+			$stateProvider.state('second',{
+				url:"/second",
+				controller: 'secondController as second',
+				templateUrl: 'templates/template2.html'
+				})
 			})	
-		}]) 
 
-	 	.controller('mainController', function(){
+		.service("greeting", function Greeting(){
+			var greeting = this;
+			greeting.message = 'Default';
+		}) 
 
- 	})
+	 	.controller('mainController', function mainController(greeting){
+	 			var main = this;
+	 			main.greeting = greeting;
+ 		})
+
+	 	.controller('secondController', function secondController(greeting){
+	 			var second = this;
+	 			second.greeting = greeting;
+ 		})
 
 app.config(['$interpolateProvider',function ($interpolateProvider){
 	$interpolateProvider.startSymbol('{[{');
