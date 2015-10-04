147640de43ea93695af233ae61d1b5f134ed6bf3
MikeWasHere <xtrjago@gmail.com>
2015-10-04 19:14:24 -0400

    added more angular to pull the JSON to angular to show on the HTML

--- Public/angular.js
+++ Public/angular.js
@@ -1,14 +1,44 @@
-var app = angular.module("pizzaSauce",["ui.router"]);
+var myApp = angular.module("myApp",["ui.router"]);
 
-app.config(['$interpolateProvider',function ($interpolateProvider){
+myApp.config(['$interpolateProvider',function ($interpolateProvider){
 	$interpolateProvider.startSymbol('{[{');
 	$interpolateProvider.endSymbol('}]}');
 }])
+
+myApp.factory('Info', function(){
+	var Info = {};
+	Info.stuff = [{
+						label = "First Name",
+						inputType = "text",
+						placeholder = "Michael",
+						pizzaSauce = "fName"
+				},
+					{
+						label = "Last Name",
+						inputType = "text",
+						placeholder = "Perez",
+						pizzaSauce = "lName"
+				},	
+
+					{
+						label = "Email",
+						inputType = "text",
+						placeholder = "myemail@host.com",
+						pizzaSauce = "email"
+				}]
+	return Info;
+})
+
+function infoCtrl($scope, Info){
+	$scope.info = Info;
+}
 	
 	.config(function config($stateProvider) {
-		url:"",
-		controller: "FirstCtrl as first",
-		templateUrl: "../views/layouts/first.html"
+		$stateProvider.state("index", {
+			url:"",
+			controller: "FirstCtrl as first",
+			templateUrl: "views/first.html"
+		})
 	})
 	
 	.controller("FirstCtrl", function FirstCtrl(){
