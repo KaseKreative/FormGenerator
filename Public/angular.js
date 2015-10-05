fc827ce40cdd347a52366cee26faacab3ad335a8
MikeWasHere <xtrjago@gmail.com>
2015-10-04 20:41:50 -0400

    stashing old code

--- Public/angular.js
+++ Public/angular.js
@@ -8,23 +8,23 @@ myApp.config(['$interpolateProvider',function ($interpolateProvider){
 myApp.factory('Info', function(){
 	var Info = {};
 	Info.stuff = [{
-						label = "First Name",
-						inputType = "text",
-						placeholder = "Michael",
-						pizzaSauce = "fName"
+						label: "First Name",
+						inputType: "text",
+						placeholder: "Michael",
+						pizzaSauce: "fName"
 				},
 					{
-						label = "Last Name",
-						inputType = "text",
-						placeholder = "Perez",
-						pizzaSauce = "lName"
+						label: "Last Name",
+						inputType: "text",
+						placeholder: "Perez",
+						pizzaSauce: "lName"
 				},	
 
 					{
-						label = "Email",
-						inputType = "text",
-						placeholder = "myemail@host.com",
-						pizzaSauce = "email"
+						label: "Email",
+						inputType: "text",
+						placeholder: "myemail@host.com",
+						pizzaSauce: "email"
 				}]
 	return Info;
 })
