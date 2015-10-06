2307cdce61736f17287740173a410832be3dfce6
KaseKreative <KaseKreative@fullsail.edu>
2015-10-06 01:18:45 -0400

    working on public folder

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var express 	= require('express'),
 	// Environmental variable
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use('/public', express.static(process.env.PWD + '/public'));
-app.use('/bower_components', express.static(process.env.PWD + '/bower_components'));
+app.use(express.static(process.env.PWD + '/public'));
+app.use( express.static(process.env.PWD + '/bower_components'));
  
 app.get('/', function (req, res) {
   res.render('home');
